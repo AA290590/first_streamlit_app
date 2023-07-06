c40cb154ae75f10216eaf121402958632e13e463
AA290590 <138759653+AA290590@users.noreply.github.com>
2023-07-06 15:07:10 +0530

    Update streamlit_app.py
    fixing the issue where fruit_selected was not defined

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,13 +8,13 @@ streamlit.text('🥗 🐔Banana oats pancakes')
 streamlit.text('🥗 Spinach Roll')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
+#my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #streamlit.dataframe(my_fruit_list)
 
 #pick the fruits they want to
 #streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
-streamlit.multiselect("pick some fruit's:",list(my_fruit_list.index),['Apple','Strawberries'])
+fruits_selected=streamlit.multiselect("pick some fruit's:",list(my_fruit_list.index),['Apple','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 
