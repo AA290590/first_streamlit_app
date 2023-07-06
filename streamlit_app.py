d0e5e879e8e9d2203f228c8eaee08b80ad3e5a8e
AA290590 <138759653+AA290590@users.noreply.github.com>
2023-07-06 15:11:31 +0530

    Update streamlit_app.py
    trying out to run

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,17 +7,17 @@ streamlit.text('🥑🍞 Oats avacado sandwich')
 streamlit.text('🥗 🐔Banana oats pancakes')
 streamlit.text('🥗 Spinach Roll')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+#my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 #my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #streamlit.dataframe(my_fruit_list)
 
 #pick the fruits they want to
 #streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
-fruits_selected=streamlit.multiselect("pick some fruit's:",list(my_fruit_list.index),['Avocado','Strawberries'])
-fruits_to_show = my_fruit_list.loc[fruits_selected]
+#fruits_selected=streamlit.multiselect("pick some fruit's:",list(my_fruit_list.index),['Avocado','Strawberries'])
+#fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 
 #display the table
-streamlit.dataframe(fruits_to_show)
+#streamlit.dataframe(fruits_to_show)
 
