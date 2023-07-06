9d4f307ea5e91a436f3d89eb9bc05be6bde78d61
AA290590 <138759653+AA290590@users.noreply.github.com>
2023-07-06 12:58:05 +0530

    Update streamlit_app.py
    added menu such as oatmeal,pancakes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
 streamlit.title('my parent new healthy diner')
 streamlit.header('breakfast menu')
+streamlit.text('blueberry oatmeal')
+streamlit.text('oats smoothie')
+streamlit.text('banana oats pancakes')
