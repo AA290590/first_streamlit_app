4e58d7737c5f3eafe22350099c9cd596459c0b2f
AA290590 <138759653+AA290590@users.noreply.github.com>
2023-07-06 13:35:45 +0530

    Update streamlit_app.py
    importing pandas and file from staging

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 streamlit.title("My MOM's New Healtiest Diner")
 streamlit.header('Breakfast Specials 🥣 🥗 🐔 🥑🍞')
 streamlit.text('🥣 🥗 Blueberry Oatmeal')
@@ -6,4 +7,7 @@ streamlit.text('🥑🍞 Oats avacado sandwich')
 streamlit.text('🥗 🐔Banana oats pancakes')
 streamlit.text('🥗 Spinach Roll')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
+
 
