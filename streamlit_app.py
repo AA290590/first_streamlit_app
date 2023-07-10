1ebd3528384d715a9a2ac6bcac4117b1171b3290
AA290590 <138759653+AA290590@users.noreply.github.com>
2023-07-10 18:48:59 +0530

    Update streamlit_app.py
    adding responses

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,9 +21,11 @@ streamlit.dataframe(fruits_to_show)
 #fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 
+streamlit.header("Fruityvice Fruit Advice!")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
+
 
 
 
