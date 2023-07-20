b0048161c8da2302fa2afa581b2554a0948feb39
AA290590 <138759653+AA290590@users.noreply.github.com>
2023-07-20 17:43:22 +0530

    Update streamlit_app.py
    indentation issue with used

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,9 +55,9 @@ if streamlit.button('fruit load list contains'):
 
 #allow end user to add fruit
 def insert_row_snowflake(new_fruit):
-     with my_cnx.cursor() as my_cur:
-     my_cur.execute("insert into fruit_load_list values(('from streamlit')")
-     return "thanks for adding" + new_fruit
+    with my_cnx.cursor() as my_cur:
+         my_cur.execute("insert into fruit_load_list values(('from streamlit')")
+         return "thanks for adding" + new_fruit
 
 add_my_fruit=streamlit.text_input('what fruit you would like to add?')
 if streamlit.button('Add fruit to list'):
