6f589f69fb8e742d326945a3fe96db47236b8a07
AA290590 <138759653+AA290590@users.noreply.github.com>
2023-07-20 17:44:48 +0530

    Update streamlit_app.py
    add_my fruit was not added new_fruit not defined

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,7 +62,7 @@ def insert_row_snowflake(new_fruit):
 add_my_fruit=streamlit.text_input('what fruit you would like to add?')
 if streamlit.button('Add fruit to list'):
         my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-        back_from_function=insert_row_snowflake(new_fruit)
+        back_from_function=insert_row_snowflake(add_my_fruit)
         streamlit.text(back_from_function)
 
 
