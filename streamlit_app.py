d7d00d3112fc3393edb25ba5842c054d23a8632e
AA290590 <138759653+AA290590@users.noreply.github.com>
2023-07-20 17:42:35 +0530

    Update streamlit_app.py
    : missing at the end of function defination

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,7 +54,7 @@ if streamlit.button('fruit load list contains'):
     streamlit.dataframe(my_data_rows)
 
 #allow end user to add fruit
-def insert_row_snowflake(new_fruit)
+def insert_row_snowflake(new_fruit):
      with my_cnx.cursor() as my_cur:
      my_cur.execute("insert into fruit_load_list values(('from streamlit')")
      return "thanks for adding" + new_fruit
